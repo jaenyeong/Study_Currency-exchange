5725de44b827d9f795f0bd19b2672d8fec17ee2a
jaenyeong <jaenyeong@Jaenyeong.local>
2022-06-05 13:38:35 +0900

    init(README.md at el): init currency exchange project with README

---
+++ src/main/java/com/jaenyeong/javacurrencyexchange/JavaCurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.jaenyeong.javacurrencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCurrencyExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCurrencyExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaenyeong/javacurrencyexchange/JavaCurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaenyeong.javacurrencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCurrencyExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ad3f72f3b48fe23dbe90cdafb6404611fa53478c
jaenyeong <jaenyeong@Jaenyeong.local>
2022-06-13 18:00:26 +0900

    feat(currency): create currency enum

---
+++ src/main/java/com/jaenyeong/javacurrencyexchange/api/domain/Currency.java
@@ -0,0 +1,17 @@
+package com.jaenyeong.javacurrencyexchange.api.domain;
+
+public enum Currency {
+    KRW("한국"),
+    JPY("일본"),
+    PHP("필리핀");
+
+    private final String country;
+
+    Currency(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}

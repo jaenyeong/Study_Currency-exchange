44a98f869d099fb7095fe99463fce91e7a7a8ebc
jaenyeong <jaenyeong@Jaenyeong.local>
2022-06-19 22:11:22 +0900

    refactor(ExchangeController): change `main` function name `mainPage`

--- src/main/java/com/jaenyeong/javacurrencyexchange/controller/ExchangeRateController.java
+++ src/main/java/com/jaenyeong/javacurrencyexchange/controller/ExchangeRateController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ExchangeRateController {
 
     @GetMapping("/")
-    public String main() {
+    public String mainPage() {
         return "exchange-rate";
     }
 }

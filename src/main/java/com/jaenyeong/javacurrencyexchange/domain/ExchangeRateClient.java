47cc32e46ff5163407340651c8bed4111643ffbf
jaenyeong <jaenyeong@Jaenyeong.local>
2022-06-19 14:44:56 +0900

    refactor(ExchangeRateClient): update `variable access modifier`, delete `@Component`

--- src/main/java/com/jaenyeong/javacurrencyexchange/domain/ExchangeRateClient.java
+++ src/main/java/com/jaenyeong/javacurrencyexchange/domain/ExchangeRateClient.java
@@ -1,25 +1,19 @@
 package com.jaenyeong.javacurrencyexchange.domain;
 
 import com.jaenyeong.javacurrencyexchange.dto.ExchangeRateDto;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Objects;
 
 import static java.time.Duration.ofSeconds;
 
-@Component
 public class ExchangeRateClient {
 
-    final String currencyLayerRequestUrl;
+    private final String currencyLayerRequestUrl;
 
-    public ExchangeRateClient(
-        @Value("${currency-layer.url}") String currencyLayerUrl,
-        @Value("${currency-layer.api-key}") String currencyKey
-    ) {
-        this.currencyLayerRequestUrl = currencyLayerUrl + "?apikey=" + currencyKey;
+    public ExchangeRateClient(final String currencyLayerUrl, final String apiKey) {
+        this.currencyLayerRequestUrl = currencyLayerUrl + "?apikey=" + apiKey;
     }
 
     private final RestTemplate restTemplate = new RestTemplateBuilder()
